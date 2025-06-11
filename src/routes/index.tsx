ad30bda4f7173d7cefa83cfb95a4c1b586b24f62
Cody <cydoentis@gmail.com>
2025-06-10 23:32:11 -0400

    Fix: updated incorrect redirect routes based on authenticated status.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    return true ? redirect({ to: "/dashboard" }) : redirect({ to: "/public" });
+    return true ? redirect({ to: "/dashboard" }) : redirect({ to: "/login" });
   },
 });
