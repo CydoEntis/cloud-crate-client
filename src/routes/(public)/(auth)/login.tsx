38080a52721c16b2b8a2fbb49b437acbd977a387
Cody <cydoentis@gmail.com>
2025-06-12 11:53:55 -0400

    implement: app logo to public layout navbar

--- src/routes/(public)/(auth)/login.tsx
+++ src/routes/(public)/(auth)/login.tsx
@@ -1,4 +1,3 @@
-// (public)/(auth)/login route
 import * as React from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { LoginForm } from "../../../features/auth/login/LoginForm";
--- src/routes/(public)/route.tsx
+++ src/routes/(public)/route.tsx
@@ -1,5 +1,6 @@
 import SidebarNavlink from "@/layouts/sidebar/SidebarNavlink";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
+import logo from "@/assets/cloud-crate-logo.png";
 
 export const Route = createFileRoute("/(public)")({
   component: RouteComponent,
@@ -8,10 +9,17 @@ export const Route = createFileRoute("/(public)")({
 function RouteComponent() {
   return (
     <section className="flex flex-col h-screen overflow-hidden">
-      <header className="border-b p-4 flex-shrink-0">
-        <nav className="flex justify-end items-center gap-4">
-          <SidebarNavlink to="/login" text="Login" />
-          <SidebarNavlink to="/register" text="Register" />
+      <header className="border-b  p-4 flex-shrink-0">
+        <nav className="flex justify-between items-center gap-4">
+          <Link to="/" className="flex justify-center items-center gap-2">
+            <img src={logo} alt="Cloud Crate Logo" className="h-10 w-10" />
+            <h3 className="font-bold text-3xl text-indigo-500">Cloud Crate</h3>
+          </Link>
+
+          <div className="flex gap-4 items-center">
+            <SidebarNavlink to="/login" text="Login" />
+            <SidebarNavlink to="/register" text="Register" />
+          </div>
         </nav>
       </header>
       <main className="flex-1 flex flex-col overflow-hidden">
