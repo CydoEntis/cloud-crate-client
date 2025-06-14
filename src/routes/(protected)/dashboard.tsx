f034eea03a854f19ea5e1b542cc827bcf27a4e6a
Cody <cydoentis@gmail.com>
2025-06-14 02:18:48 -0400

    implement: Temp dashboard page with display card.

--- src/routes/(protected)/dashboard.tsx
+++ src/routes/(protected)/dashboard.tsx
@@ -1,9 +1,24 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/(protected)/dashboard')({
+export const Route = createFileRoute("/(protected)/dashboard")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_protected/dashboard"!</div>
+  return (
+    <section className="p-4">
+      <div className="max-w-md mx-auto">
+        <div className="bg-white rounded-2xl shadow-lg overflow-hidden flex flex-col min-h-64 border">
+          {/* Main content */}
+          <div className="flex-1 p-4 bg-yellow-100">
+            <p className="text-lg font-semibold text-yellow-900">Main content here</p>
+            <p>Stuff inside the card body</p>
+          </div>
+
+          {/* Footer */}
+          <div className="bg-yellow-300 p-4 text-sm text-yellow-900 border-t mt-auto">Footer content</div>
+        </div>
+      </div>
+    </section>
+  );
 }
