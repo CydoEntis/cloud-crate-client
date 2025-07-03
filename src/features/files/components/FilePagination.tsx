5ec1010f3a50078c17434ccf8633f74918b795f8
Cody <cydoentis@gmail.com>
2025-07-03 11:08:49 -0400

    implement: FilePagination

--- src/features/files/components/FilePagination.tsx
+++ src/features/files/components/FilePagination.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
 
-type Props = {}
+type Props = {
+  page: number;
+  pageSize: number;
+  totalCount: number;
+  onPageChange: (page: number) => void;
+};
 
-function FilePagination({}: Props) {
+export default function FilePagination({ page, pageSize, totalCount, onPageChange }: Props) {
+  const totalPages = Math.ceil(totalCount / pageSize);
+
   return (
-    <div>FilePagination</div>
-  )
+    <div className="mt-4 flex justify-between items-center px-4 pb-4">
+      <span>
+        Page {page} of {totalPages}
+      </span>
+      <div className="space-x-2">
+        <Button onClick={() => onPageChange(page - 1)} disabled={page === 1}>
+          Previous
+        </Button>
+        <Button onClick={() => onPageChange(page + 1)} disabled={page >= totalPages}>
+          Next
+        </Button>
+      </div>
+    </div>
+  );
 }
-
-export default FilePagination
