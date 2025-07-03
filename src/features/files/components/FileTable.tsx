57058b6c19cb51e63e436108a5136abbefd7ff95
Cody <cydoentis@gmail.com>
2025-07-03 11:07:21 -0400

    implement: FIleTable renderer component.

--- src/features/files/components/FileTable.tsx
+++ src/features/files/components/FileTable.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useReactTable, getCoreRowModel, type ColumnDef } from "@tanstack/react-table";
+import { Table, TableBody } from "@/components/ui/table";
+import FileTableHeader from "./FileTableHeader";
+import FileTableRow from "./FileTableRow";
+import type { StoredFile } from "@/features/files/types";
 
-type Props = {}
+type FileTableProps = {
+  data: StoredFile[];
+  columns: ColumnDef<StoredFile, any>[];
+  onRowClick?: (file: StoredFile) => void;
+};
 
-function FileTable({}: Props) {
+export default function FileTable({ data, columns, onRowClick }: FileTableProps) {
+  const table = useReactTable<StoredFile>({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
-    <div>FileTable</div>
-  )
+    <Table>
+      <FileTableHeader table={table} />
+      <TableBody>
+        {table.getRowModel().rows.map((row) => (
+          <FileTableRow key={row.id} row={row} onClick={onRowClick} />
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
-
-export default FileTable
