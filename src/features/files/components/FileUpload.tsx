dab59d843bee0d3059668b09f83ec7210685304c
Cody <cydoentis@gmail.com>
2025-08-04 20:33:00 -0400

    refactor: Updated FileUpload to match theme.

--- src/features/files/components/FileUpload.tsx
+++ src/features/files/components/FileUpload.tsx
@@ -68,16 +68,16 @@ function FileUpload({ crateId, folderId }: FileUploadProps) {
 
       <div
         onClick={() => inputRef.current?.click()}
-        className="cursor-pointer p-6 w-full flex flex-col justify-center items-center gap-5 rounded-xl border-2 border-indigo-700 my-6 outline-4 outline-indigo-200 hover:bg-indigo-50 transition"
+        className="cursor-pointer p-6 w-full flex flex-col justify-center items-center gap-5 rounded-xl border-2 border-input my-6 outline-4 outline-input/60 outline-offset-2 hover:bg-input/10 transition"
       >
-        <div className="rounded-full bg-indigo-100 p-4">
-          <Upload className="text-indigo-700" />
+        <div className="rounded-full bg-input/80 p-4">
+          <Upload className="text-primary" />
         </div>
-        <p className="text-center">
-          <span className="text-indigo-700 font-medium">Click here</span> to upload or{" "}
-          <span className="text-indigo-700 font-medium">drag and drop</span> your file
+        <p className="text-foreground">
+          <span className="text-primary font-medium">Click here</span> to upload or{" "}
+          <span className="text-primary font-medium">drag and drop</span> your file
         </p>
-        <p className="text-gray-400 text-sm text-center">Supported Formats: SVG, JPG, PNG (10mb each)</p>
+        <p className="text-muted-foreground text-sm text-center">Supported Formats: SVG, JPG, PNG (10mb each)</p>
       </div>
 
       {formState.errors.files && (
